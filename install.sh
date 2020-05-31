2e9e30e7ffd9ff5670688990e14b4b366716e360
Otto Urpelainen <oturpe@iki.fi>
2020-05-31 22:24:52 +0300

    Lokinpitoa asennuskomentoon

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
+echo "Asennetaan riippuvuudet."
 dnf --assumeyes install make gcc alsa-lib-devel libgpiod-devel
+
+echo "Konfiguroidaan systemd."
 cp systemd/montauk-chair-interface.service /lib/systemd/system/
 systemctl daemon-reload
 systemctl enable montauk-chair-interface.service
+
+echo "Konfiguraatio valmis. Käynnistä laite uudelleen, jotta asetukset astuvat voimaan."
